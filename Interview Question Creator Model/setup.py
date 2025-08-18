7015d1acc348019777e02af073820459fcdad337
Abhijit.D <abhijit.d88@gmail.com>
2025-08-18 11:11:03 +0530

    Commit

---
+++ Interview Question Creator Model/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version= '0.0.0',
+    author="Abhijit D",
+    author_email="abhijit.d88@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
